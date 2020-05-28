cf5ea7702af675e76f2f7e023013024d422f1732
Leonardo Ariel Sequeira <lsequeira@baufest.com>
2020-05-27 22:46:12 -0300

    Subo Punto 4:
    *Agrego input de imagen al form
    *Valido el formato de la imagen
    *La muevo de directorio

---
+++ Punto_4/views/new.turno.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sistema de Turnos Medicos</title>
+</head>
+<body>
+    <header>
+        <h1>Alta de turno:</h1>
+        <?php include 'views/nav.view.php'; ?>
+    </header>
+    <main>
+        <?php include 'views/new.turno.form.view.php'; ?>
+    </main>
+</body>
+</html>
